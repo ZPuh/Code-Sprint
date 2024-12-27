8175879c3b638342b08652712893fe3fcf412ec8
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2024-12-27 19:21:15 +0800

    Print

---
+++ days001.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Task 1 : Mencetak Hello World
+    cout << "Hello World" << endl;
+
+    // Task 2 : Biodata
+    cout << "Muhammad Zuhdi" << endl;
+    cout << "D0223320" << endl;
+    cout << "Informatika" << endl;
+
+    // Task 3 : Menggunaka \n
+    cout << "baris 1\nbaris 2\nbaris 3";
+
+    return 0;
+}
